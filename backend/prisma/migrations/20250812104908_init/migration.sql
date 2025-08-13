fd5f6edfe86ad8343096544e65850ff0200f8133
golfstrimmar <yushinbox@gmail.com>
2025-08-13 15:26:29 +0200

    oiuyt

--- backend/prisma/migrations/20250812104908_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."Message" (
-    "id" SERIAL NOT NULL,
-    "text" TEXT NOT NULL,
-    "author" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
-);
--- backend/prisma/migrations/20250812110331_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `author` on the `Message` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Message" DROP COLUMN "author";
