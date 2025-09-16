a8ffdc32cc998f47eaa65d3960573b954bccd83c
golfstrimmar <yushinbox@gmail.com>
2025-09-16 15:30:09 +0200

    +++

--- backend/index.js
+++ backend/index.js
@@ -3,50 +3,36 @@ import http from "http";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { WebSocketServer } from "ws";
-import { useServer } from "graphql-ws/use/ws";
 import bodyParser from "body-parser";
 import cors from "cors";
 
 import { typeDefs } from "./graphql/schema.js";
 import { resolvers } from "./graphql/resolvers.js";
 
+// Создаём схему
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-// Создаём Apollo Server
+// Apollo Server
 const server = new ApolloServer({ schema });
 await server.start();
 
 const app = express();
+
+// ✅ CORS один раз для /graphql
 app.use(
-  // "/graphql",
+  "/graphql",
   cors({
-    origin: [
-      "http://localhost:3002", // локалка
-      // "https://react-2024-blog.vercel.app", // твой фронт
-      // "https://твой-проект.vercel.app", // будущий продакшн
-    ],
+    origin: ["http://localhost:3002"], // фронт локальный + прод
     credentials: true,
   }),
   bodyParser.json(),
   expressMiddleware(server)
 );
 
-// Создаём HTTP сервер
+// HTTP сервер
+const PORT = process.env.PORT || 4000;
 const httpServer = http.createServer(app);
 
-// Создаём WS сервер для подписок
-const wsServer = new WebSocketServer({
-  server: httpServer,
-  path: "/graphql",
-});
-useServer({ schema }, wsServer);
-
-const PORT = process.env.PORT || 4000;
-// httpServer.listen(PORT, () => {
-//   console.log(`🚀 Query/Mutation: https://ulon.up.railway.app`);
-//   console.log(`🚀 Subscriptions: wss://ulon.up.railway.app/graphql`);
-// });
 httpServer.listen(PORT, () => {
   console.log(`🚀 GraphQL server running on port ${PORT}`);
 });
